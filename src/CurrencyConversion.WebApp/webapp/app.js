24122928c4a1d46394ed58f51241d27b6d621648
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2017-09-19 18:47:54 +0300

    Base layout

---
+++ src/CurrencyConversion.WebApp/webapp/app.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('CurrencyConversion', ['ngRoute'])
+        .config(function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/webapp/html/pages/conversion/index.html'
+                })
+                .when('/requirements', {
+                    templateUrl: '/webapp/html/pages/requirements/index.html'
+                })
+                .when('/contact', {
+                    templateUrl: '/webapp/html/pages/contact/index.html'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        });
+})();
